5fa69a96fdf72983bd98a0879e5192d9c99b20ba
Alina Safina <lina.zumba@mail.ru>
2022-10-21 17:44:30 +0300

    feat: add popup to both pages

--- shelter/pets.js
+++ shelter/pets.js
@@ -1,7 +1,8 @@
-[
+const data = JSON.parse(`[
   {
+    "id": "1jennifer",
     "name": "Jennifer",
-    "img": "../../assets/images/jennifer.png",
+    "img": "jennifer.png",
     "type": "Dog",
     "breed": "Labrador",
     "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
@@ -11,8 +12,9 @@
     "parasites": ["none"]
   },
   {
+    "id": "2sophia",
     "name": "Sophia",
-    "img": "../../assets/images/sophia.png",
+    "img": "sophia.png",
     "type": "Dog",
     "breed": "Shih tzu",
     "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
@@ -22,8 +24,9 @@
     "parasites": ["none"]
   },
   {
+    "id": "3woody",
     "name": "Woody",
-    "img": "../../assets/images/woody.png",
+    "img": "woody.png",
     "type": "Dog",
     "breed": "Golden Retriever",
     "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
@@ -33,8 +36,9 @@
     "parasites": ["none"]
   },
   {
+    "id": "4scarlett",
     "name": "Scarlett",
-    "img": "../../assets/images/scarlett.png",
+    "img": "scarlett.png",
     "type": "Dog",
     "breed": "Jack Russell Terrier",
     "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
@@ -44,8 +48,9 @@
     "parasites": ["none"]
   },
   {
+    "id": "5katrine",
     "name": "Katrine",
-    "img": "../../assets/images/katrine.png",
+    "img": "katrine.png",
     "type": "Cat",
     "breed": "British Shorthair",
     "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
@@ -55,8 +60,9 @@
     "parasites": ["none"]
   },
   {
+    "id": "6timmy",
     "name": "Timmy",
-    "img": "../../assets/images/timmy.png",
+    "img": "timmy.png",
     "type": "Cat",
     "breed": "British Shorthair",
     "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
@@ -66,8 +72,9 @@
     "parasites": ["none"]
   },
   {
+    "id": "7freddie",
     "name": "Freddie",
-    "img": "../../assets/images/freddie.png",
+    "img": "freddie.png",
     "type": "Cat",
     "breed": "British Shorthair",
     "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
@@ -77,8 +84,9 @@
     "parasites": ["none"]
   },
   {
+    "id": "8charly",
     "name": "Charly",
-    "img": "../../assets/images/charly.png",
+    "img": "charly.png",
     "type": "Dog",
     "breed": "Jack Russell Terrier",
     "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
@@ -87,4 +95,4 @@
     "diseases": ["deafness", "blindness"],
     "parasites": ["lice", "fleas"]
   }
-]
+]`);
--- shelter/script.js
+++ shelter/script.js
@@ -10,7 +10,7 @@ const menuListItems = navMenu.querySelectorAll('li');
 
 burger.addEventListener('click', addAnimatedClass);
 burger.addEventListener('click', toggleClasses);
-overlay.addEventListener('click', toggleClasses);
+overlay.addEventListener('click', removeActiveMenuClasses);
 
 Array.from(menuListItems).forEach((item) =>
   item.addEventListener('click', toggleClasses)
@@ -28,4 +28,67 @@ function toggleClasses() {
   burger.classList.toggle('active-menu');
   navMenu.classList.toggle('active-menu');
 }
-console.log(header, burger);
+
+function removeActiveMenuClasses() {
+  body.classList.remove('active-menu');
+  header.classList.remove('active-menu');
+  logo.classList.remove('active-menu');
+  burger.classList.remove('active-menu');
+  navMenu.classList.remove('active-menu');
+}
+
+//popup
+
+const petContainer = document.querySelector('.pet-container');
+const popup = document.querySelector('.popup');
+const popupImg = popup.querySelector('img');
+const popupTitle = popup.querySelector('.content__title');
+const popupSubtitle = popup.querySelector('.content__subtitle');
+const popupDescr = popup.querySelector('.content__descr');
+const petDiseases = popup.querySelector('[data-id="diseases"]');
+const petAge = popup.querySelector('[data-id="age"]');
+const petInoculations = popup.querySelector('[data-id="inoculations"]');
+const petParasites = popup.querySelector('[data-id="parasites"]');
+const popupCloseBtn = popup.querySelector('.popup__closeBtn');
+
+// const learnMoreBtns = petContainer.querySelectorAll('button');
+
+petContainer.addEventListener('click', openPopup);
+overlay.addEventListener('click', (event) => {
+  if (event.target === overlay || event.target === popupCloseBtn) {
+    removePopupClasses();
+  }
+});
+popupCloseBtn.addEventListener('click', removePopupClasses);
+
+function openPopup(event) {
+  const eventTarget = event.target;
+
+  if (eventTarget.nodeName === 'BUTTON') {
+    popup.classList.remove('animated');
+
+    const id = eventTarget.parentNode.id;
+    const currPet = data.find((pet) => pet.id === id);
+
+    popupImg.src = `${imgSrc}/${currPet.img}`;
+    popupImg.alt = currPet.name;
+    popupTitle.innerHTML = currPet.name;
+    popupSubtitle.innerHTML = `${currPet.type} - ${currPet.breed}`;
+    popupDescr.innerHTML = currPet.description;
+    petAge.innerHTML = currPet.age;
+    petDiseases.innerHTML = currPet.diseases;
+    petInoculations.innerHTML = currPet.inoculations;
+    petParasites.innerHTML = currPet.parasites;
+
+    popup.classList.add('opened-popup');
+    body.classList.toggle('opened-popup');
+  }
+}
+
+function removePopupClasses() {
+  popup.classList.add('animated');
+  setTimeout(() => {
+    body.classList.remove('opened-popup');
+    popup.classList.remove('opened-popup');
+  }, 500);
+}
